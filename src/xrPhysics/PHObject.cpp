69deed0973ae1b2b9f2c23e966f0e9a9a46a1562
ZeroTheAbsolute <shikyo13@gmail.com>
2026-09-07 11:31:06 -0500

    Fix MSVC static x64 build and physics iterator linkage

--- src/xrPhysics/PHObject.cpp
+++ src/xrPhysics/PHObject.cpp
@@ -266,6 +266,6 @@ void CPHUpdateObject::Deactivate()
 {
     if (!b_activated)
         return;
-    ph_world->RemoveUpdateObject(this);
+    ph_world->RemoveUpdateObject(PH_UPDATE_OBJECT_I(this));
     b_activated = false;
 }
